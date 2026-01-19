f77f6840348162d896170767a068096d4d4b4e00
Balestro-Denis <denisbalestro@gmail.com>
2026-01-19 20:03:51 +0100

    Add funtions

---
+++ functions.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+
+// Creo la riga che conterrà le card
+let rowHTML = '<div class="row mb-4">';
+
+// Ciclo for sui membri del team
+for (let i = 0; i < teamMembers.length; i++) {
+  const member = teamMembers[i];
+}
